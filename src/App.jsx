7aa2d136319c041d5e5c7010ef9acafc441d9b38
Cornelius Emase <108942025+Lochipi@users.noreply.github.com>
2023-11-09 13:18:21 +0000

    finished customizing app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-/**
- * Application component
- *
- * To contain application wide settings, routes, state, etc.
- */
-
 import React from "react";
 
 import About from "./Components/About";
@@ -14,24 +8,15 @@ import Portfolio from "./Components/Portfolio";
 
 import "./styles.css";
 
-/**
- * This object represents your information. The project is set so that you
- * only need to update these here, and values are passed a properties to the
- * components that need that information.
- *
- * Update the values below with your information.
- *
- * If you don't have one of the social sites listed, leave it as an empty string.
- */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
+  name: "Cornelius Emase",
+  title: "Software Engineer & Content Creator",
+  email: "corneliuslochipi@gmail.com",
+  gitHub: "https://github.com/Lochipi",
+  instagram: "",
+  linkedIn: "satyanhttps://www.linkedin.com/in/cornelius-emase/adella",
+  medium: "https://medium.com/@corneliuslochipi",
+  twitter: "https://twitter.com/EmaseLC",
   youTube: "Code",
 };
 
